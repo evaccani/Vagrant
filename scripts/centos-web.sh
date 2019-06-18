93505c0edcebde278a842670061f48df057219d3
Edgar Vaccani <edgard.vaccani@italtel.com>
2019-06-18 15:36:12 -0300

    Adding files for multi-vm setup

---
+++ scripts/centos-web.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Firewall Service Stop
+service iptables stop
+chkconfig iptables off
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
